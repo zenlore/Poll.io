0375a12ee5fd79e0a13f1937528b1dfb2c33c944
at235am <at235am@gmail.com>
2019-04-30 20:21:23 -0700

    setting up the submit logic; needa add GPS

--- app/src/main/java/cecs343/pollio/NewPollActivity.java
+++ app/src/main/java/cecs343/pollio/NewPollActivity.java
@@ -2,8 +2,10 @@ package cecs343.pollio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import java.util.ArrayList;
 
@@ -14,6 +16,9 @@ public class NewPollActivity extends AppCompatActivity {
     private ListView listViewOfOptions;
     private PollOptionsAdapter pollOptionsAdapter;
     public ArrayList<EditModel> optionsList;
+
+    private PollItem pollItem;
+    private EditText editTextPollTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,13 +31,30 @@ public class NewPollActivity extends AppCompatActivity {
         listViewOfOptions = (ListView) findViewById(R.id.poll_options);
         listViewOfOptions.setAdapter(pollOptionsAdapter);
 
+        editTextPollTitle = (EditText) findViewById(R.id.poll_title_question);
+
         submitBtn = (Button) findViewById(R.id.submit);
         submitBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                for (int i = 0; i < optionsList.size(); i++){
-                    System.out.println(optionsList.get(i).getEditTextValue());
+                pollItem = new PollItem(editTextPollTitle.getText().toString(), false, getIDfromDB());
+
+                for (int i = 0; i < optionsList.size(); i++) {
+                    pollItem.addPollOption(new PollOption(optionsList.get(i).getEditTextValue(), 0));
                 }
+
+                // TODO: add GPS coordinates to pollItem:
+
+
+                // logging pollItem to see if contents are correct:
+                Log.i("title", pollItem.getTitle());
+                for (int i = 0; i < pollItem.getOptions().size(); i++) {
+                    Log.i(String.valueOf(i+1), pollItem.getOptions().get(i).getText());
+                }
+                Log.i("size", String.valueOf(pollItem.getNumOptions()));
+
+                // destroy this instance of NewPollActivity:
+                finish();
             }
         });
 
@@ -40,7 +62,7 @@ public class NewPollActivity extends AppCompatActivity {
         addNewOptionBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                if(optionsList.size() < 8) {
+                if(optionsList.size() < 5) {
                     EditModel em = new EditModel();
                     optionsList.add(em);
                     listViewOfOptions.setAdapter(new PollOptionsAdapter(NewPollActivity.this, optionsList));
@@ -52,10 +74,16 @@ public class NewPollActivity extends AppCompatActivity {
 
     private ArrayList<EditModel> populateList(){
         ArrayList<EditModel> list = new ArrayList<>();
-        for(int i = 0; i < 4; i++){
+        for(int i = 0; i < 2; i++){
             EditModel editModel = new EditModel();
             list.add(editModel);
         }
         return list;
     }
+
+    private int getIDfromDB(){
+        return 1337;
+    }
+
+
 }
