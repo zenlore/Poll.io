5025fa5b56bd41ca564fd3448825c65342b2f0ce
spidervoelker <spidervoelker@gmail.com>
2019-05-03 20:19:46 -0700

    users can't type pipes into new poll fields

--- app/src/main/java/cecs343/pollio/AccountFragment.java
+++ app/src/main/java/cecs343/pollio/AccountFragment.java
@@ -39,14 +39,7 @@ import java.util.ArrayList;
  */
 public class AccountFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener,
         GoogleMap.OnMyLocationClickListener {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
     private Button logoutButton;
     private Button forgotPassButton;
     private TextView displaynameTextView;
@@ -69,16 +62,12 @@ public class AccountFragment extends Fragment implements OnMapReadyCallback, Goo
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment AccountFragment.
      */
     // TODO: Rename and change types and number of parameters
     public static AccountFragment newInstance(String param1, String param2) {
         AccountFragment fragment = new AccountFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
